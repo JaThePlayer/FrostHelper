18e7606d652fadcb72c7c187e20c704adfb7e4a2
JaThePlayer <50085307+JaThePlayer@users.noreply.github.com>
2026-05-14 21:36:18 +0200

    Fixed OnFlagActivator crash with triggerOnRoomBegin and delay.

--- Code/FrostHelper/Triggers/Activator/OnFlag.cs
+++ Code/FrostHelper/Triggers/Activator/OnFlag.cs
@@ -13,11 +13,19 @@ internal sealed class OnFlagActivator : BaseActivator {
         Add(new FlagListener(data.Attr("flag"), OnFlag, data.Bool("mustChange", false), data.Bool("triggerOnRoomBegin", false)));
         TargetValue = data.Bool("targetState", true);
 
-        // reparse with different default value to replicate old behaviour
+        // reparse with different default value to replicate old behavior
         ActivateAfterDeath = data.Bool("activateAfterDeath", true);
     }
 
-    public void OnFlag(Session session, string? flag, bool value) {
+    public override void Awake(Scene scene) {
+        // OnFlag might get called during Awake's EntityAwake() calling loop,
+        // causing a crash if a delay is set on the activator set to trigger on room begin (creating a Coroutine component)
+        Components.LockMode = ComponentList.LockModes.Locked;
+        base.Awake(scene);
+        Components.LockMode = ComponentList.LockModes.Open;
+    }
+
+    private void OnFlag(Session session, string? flag, bool value) {
         if (value == TargetValue) {
             var player = Scene?.Tracker.GetEntity<Player>();
             if (player != null || ActivateAfterDeath)
